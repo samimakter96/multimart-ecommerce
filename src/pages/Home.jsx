e29d44b7a56ea0ee65b6b293db5806b449c56598
samim akter <samimakter9641@gmail.com>
2024-07-08 09:47:25 +0530

    Services added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
-import heroImg from '../assets/images/hero-img.png'
-import '../styles/Home.css'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Services from "../services/Services";
 
 const Home = () => {
-
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
   return (
-    <section className='hero__section'>
-      <Container>
-        <Row>
-          <Col lg='6' md='6'>
-            <div className="hero__content">
-              <p className="hero__subtitle">Trending product in {year}</p>
-              <h2>Make Your Interior More Minimalistic & Modern</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, deleniti molestias. Nostrum numquam dolorem ducimus, labore impedit placeat minima est.</p>
-              <motion.button whileTap={{scale: 1.2}} className="buy__btn"><Link to='/shop'>SHOP NOW</Link></motion.button>
-            </div>
-          </Col>
-          
-          <Col lg='6' md='6'>
-            <div className="hero__img">
-              <img src={heroImg} alt="" />
-            </div>
-          </Col>
+    <>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending product in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Sapiente, deleniti molestias. Nostrum numquam dolorem ducimus,
+                  labore impedit placeat minima est.
+                </p>
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
 
-        </Row>
-      </Container>
-    </section>
-  )
-}
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
 
-export default Home
+      <Services />
+    </>
+  );
+};
+
+export default Home;
