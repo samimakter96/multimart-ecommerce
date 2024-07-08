53469e824653fa2054a37ed0ed7ece1c5214e50e
samim akter <samimakter9641@gmail.com>
2024-07-08 11:56:03 +0530

    New Arrivals

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,8 @@ import Clock from "../components/UI/Clock";
 const Home = () => {
   const [trendingProducts, setTrendingProducts] = useState([]);
   const [bestSalesProducts, setBestSalesProducts] = useState([]);
+  const  [mobileProducts, setMobileProducts] = useState([]);
+  const  [wirelessProducts, setWirelessProducts] = useState([]);
 
   const year = new Date().getFullYear();
 
@@ -24,8 +26,20 @@ const Home = () => {
     const filteredBestSalesProducts = products.filter(
       (item) => item.category === "sofa"
     );
+
+    const filteredMobileProducts = products.filter(
+      (item) => item.category === "mobile"
+    );
+
+    const filteredWirelessProducts = products.filter(
+      (item) => item.category === "wireless"
+    );
+
     setTrendingProducts(filteredTrendingProducts);
     setBestSalesProducts(filteredBestSalesProducts);
+    setMobileProducts(filteredMobileProducts);
+    setWirelessProducts(filteredWirelessProducts);
+
   }, []);
 
   return (
@@ -96,6 +110,18 @@ const Home = () => {
             <Col lg="6" md="6" className="text-end">
               <img src={counterImg} alt="" />
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="new__arrivals">
+        <Container>
+          <Row>
+          <Col lg="12" className="text-center">
+              <h2 className="section__title">New Arrivals</h2>
+            </Col>
+            <ProductList data={mobileProducts}/>
+            <ProductList data={wirelessProducts}/>
           </Row>
         </Container>
       </section>
