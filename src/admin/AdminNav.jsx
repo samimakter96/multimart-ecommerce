73a6dbd2a6bcd6cf5658f18ec33b829adf6f4b13
samim akter <samimakter9641@gmail.com>
2024-07-10 00:51:17 +0530

    Admin Nav created

--- src/admin/AdminNav.jsx
+++ src/admin/AdminNav.jsx
@@ -1,8 +1,88 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Nav, Row } from "reactstrap";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+
+import useAuth from "../customHooks/useAuth";
+import "../styles/AdminNav.css";
+import { NavLink } from "react-router-dom";
+
+const admin__nav = [
+  {
+    display: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    display: "All-Products",
+    path: "/dashboard/all-products",
+  },
+  {
+    display: "Orders",
+    path: "/dashboard/orders",
+  },
+  {
+    display: "Users",
+    path: "/dashboard/users",
+  },
+];
 
 const AdminNav = () => {
-  return <div>AdminNav</div>;
+  const { currentUser } = useAuth();
+
+  return (
+    <>
+      <header className="admin__header">
+        <div className="admin__nav-top">
+          <Container>
+            <div className="admin__nav-wrapper-top">
+              <div className="logo">
+                <h2>Multimart</h2>
+              </div>
+              <div className="search__box">
+                <input type="text" placeholder="Search...." />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+              <div className="admin__nav-top-right">
+                <span>
+                  <IoMdNotificationsOutline size={"20px"} />
+                </span>
+                <span>
+                  <IoSettingsOutline size={"20px"} />
+                </span>
+                <img src={currentUser.photoURL} alt="" />
+              </div>
+            </div>
+            <Row></Row>
+          </Container>
+        </div>
+      </header>
+
+      <section className="admin__menu p-0">
+        <Container>
+          <Row>
+            <div className="admin__navigation">
+              <ul className="admin__menu-list">
+                {admin__nav.map((item, index) => (
+                  <li key={index} className="admin__menu-item">
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__admin-menu" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default AdminNav;
