5579ce63b44d7aea6697ca05d203e602ddf5ba3a
samim akter <samimakter9641@gmail.com>
2024-07-10 00:12:05 +0530

    Admin Dashboard
    stracture all the folder

---
+++ src/admin/AdminNav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const AdminNav = () => {
+  return <div>AdminNav</div>;
+};
+
+export default AdminNav;
--- src/routers/ProtectedRoute.jsx
+++ src/routers/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import useAuth from "../customHooks/useAuth";
 import { Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   const { currentUser } = useAuth();
-  console.log(currentUser)
+  console.log(currentUser);
 
-  return currentUser ? children : <Navigate to="/login" />;
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
